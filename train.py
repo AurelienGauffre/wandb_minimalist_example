9a3bac7799021c44cd68bccfe51d9eaf0cffcba4
gauffrea <aurelien.gauffre@gmail.com>
2023-11-07 13:17:58 +0100

    Add subset cifar 10 to speed up training

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 import argparse
 import torch
-import torch.nn as nn
-import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Subset
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.models import resnet18
+import random
 from omegaconf import OmegaConf
 import wandb
-from torchvision.models import resnet18
 
-# Define the network architecture
+DATASET_FRACTION = .2 # take 20% of the dataset
 class ModifiedResNet18(nn.Module):
     def __init__(self, num_classes=10):
         super(ModifiedResNet18, self).__init__()
@@ -19,36 +21,47 @@ class ModifiedResNet18(nn.Module):
     def forward(self, x):
         return self.resnet(x)
 
-def train(args=None, use_wandb_sweep=True):
-    if use_wandb_sweep:
+
+def train(config=None):
+    print(f"Training on {DATASET_FRACTION * 100}% of Cifar10 dataset")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Training on device: {device}")
+    if config is None:
         run = wandb.init()
-        args = run.config
+        config = run.config
     else:
-        args = OmegaConf.create(args)
+        config = OmegaConf.create(config)
+        wandb.init(project="cifar10_resnet18_subset", config=config) # Add argument name=config.run_name if you want to name the run
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    transform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                              shuffle=True, num_workers=2)
+    # Load the full CIFAR-10 dataset
+    full_trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    full_testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
-                                             shuffle=False, num_workers=2)
+    # Select a subset (10%) of the dataset
+    subset_indices_train = random.sample(range(len(full_trainset)), int(DATASET_FRACTION * len(full_trainset)))
+    subset_indices_test = random.sample(range(len(full_testset)), int(DATASET_FRACTION * len(full_testset)))
 
-    model = ModifiedResNet18(num_classes=10)
+    trainset = Subset(full_trainset, subset_indices_train)
+    testset = Subset(full_testset, subset_indices_test)
+
+    trainloader = DataLoader(trainset, batch_size=config.batch_size, shuffle=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size=config.batch_size, shuffle=False, num_workers=2)
+
+    model = ModifiedResNet18(num_classes=10).to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=config.lr, momentum=0.9)
 
-    for epoch in range(args.epochs):
+    for epoch in range(config.epochs):
         model.train()
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data
+        for i, data in enumerate(trainloader):
+            inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
@@ -62,7 +75,7 @@ def train(args=None, use_wandb_sweep=True):
         test_loss = 0
         with torch.no_grad():
             for data in testloader:
-                images, labels = data
+                images, labels = data[0].to(device), data[1].to(device)
                 outputs = model(images)
                 loss = criterion(outputs, labels)
                 test_loss += loss.item()
@@ -71,26 +84,24 @@ def train(args=None, use_wandb_sweep=True):
                 correct += (predicted == labels).sum().item()
 
         accuracy = correct / total
-        metrics = {'epoch': epoch, 'train_loss': running_loss / len(trainloader), 'test_loss': test_loss / len(testloader), 'accuracy': accuracy}
+        metrics = {'epoch': epoch, 'train/loss': running_loss / len(trainloader),
+                   'test/loss': test_loss / len(testloader), 'test/accuracy': accuracy}
+        print(metrics)
+        wandb.log(metrics)
 
-        if use_wandb_sweep:
-            wandb.log(metrics)
-
-    if use_wandb_sweep:
-        wandb.finish()
+    wandb.finish()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Training configurations.')
     parser.add_argument('--config', type=str, default='config.yaml', help='Path to the config file.')
     args_command_line = parser.parse_args()
 
-    USE_WANDB_SWEEP = "sweep" in args_command_line.config  # wandb sweep config files must contain "sweep" in their name
-    config_dict = OmegaConf.to_container(OmegaConf.load(args_command_line.config), resolve=True)  # Convert the yaml file into a classical python dictionary
+    USE_WANDB_SWEEP = "sweep" in args_command_line.config
+    config_dict = OmegaConf.to_container(OmegaConf.load(args_command_line.config), resolve=True)
 
     if not USE_WANDB_SWEEP:
-        train(config_dict, False)
+        train(config_dict)
     else:
         sweep_id = wandb.sweep(sweep=config_dict, project="cifar10_resnet18")
         print(f"Created a wandb Sweep with id: {sweep_id}")
         wandb.agent(sweep_id=sweep_id, function=train)
-
