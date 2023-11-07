4fc848139adadd8e3bfaaaa91652fb82d2f11087
gauffrea <aurelien.gauffre@gmail.com>
2023-11-07 16:45:05 +0100

    Add comments

--- train.py
+++ train.py
@@ -10,11 +10,15 @@ import random
 from omegaconf import OmegaConf
 import wandb
 
-DATASET_FRACTION = .2 # take 20% of the dataset
-class ModifiedResNet18(nn.Module):
+DATASET_FRACTION = .2  # take 20% of the dataset to speed up training
+
+
+class ResNet18Cifar(nn.Module):
     def __init__(self, num_classes=10):
-        super(ModifiedResNet18, self).__init__()
+        super(ResNet18Cifar, self).__init__()
         self.resnet = resnet18(pretrained=False)
+        # Classical way to adjust the first convolutional layer for small images
+        # since the original ResNet18 is trained on ImageNet
         self.resnet.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
 
@@ -23,37 +27,34 @@ class ModifiedResNet18(nn.Module):
 
 
 def train(config=None):
+    if config is None:  # it means we are using a wandb sweep, so the config has to be loaded from wandb
+        run = wandb.init()
+        config = run.config
+    else:  # it means we are using directly the given config file, and we upload it to wandb to keep a record of the training parameters
+        config = OmegaConf.create(config)
+        wandb.init(project="cifar10_resnet18",
+                   config=config)  # Add as argument name=config.run_name if you want to name the run
+
     print(f"Training on {DATASET_FRACTION * 100}% of Cifar10 dataset")
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Training on device: {device}")
-    if config is None:
-        run = wandb.init()
-        config = run.config
-    else:
-        config = OmegaConf.create(config)
-        wandb.init(project="cifar10_resnet18_subset", config=config) # Add argument name=config.run_name if you want to name the run
-
     transform = transforms.Compose([
         transforms.Resize((32, 32)),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
 
-    # Load the full CIFAR-10 dataset
+    # Load the full CIFAR-10 dataset and select a subset of it
     full_trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     full_testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-
-    # Select a subset (10%) of the dataset
     subset_indices_train = random.sample(range(len(full_trainset)), int(DATASET_FRACTION * len(full_trainset)))
     subset_indices_test = random.sample(range(len(full_testset)), int(DATASET_FRACTION * len(full_testset)))
-
     trainset = Subset(full_trainset, subset_indices_train)
     testset = Subset(full_testset, subset_indices_test)
-
     trainloader = DataLoader(trainset, batch_size=config.batch_size, shuffle=True, num_workers=2)
     testloader = DataLoader(testset, batch_size=config.batch_size, shuffle=False, num_workers=2)
 
-    model = ModifiedResNet18(num_classes=10).to(device)
+    model = ResNet18Cifar(num_classes=10).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=config.lr, momentum=0.9)
 
@@ -90,6 +91,7 @@ def train(config=None):
         wandb.log(metrics)
 
     wandb.finish()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Training configurations.')
